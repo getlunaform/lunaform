86be96f472376bbe9fcb79c621a62a02722d1545
Drew J. Sonne <drew.sonne@gmail.com>
2017-11-25 22:23:28 +0000

    Added basic ping and interface test

--- backend/database/json_test.go
+++ backend/database/json_test.go
@@ -1,12 +1,22 @@
 package database
 
-import "testing"
+import (
+	"github.com/zeebox/terraform-server/backend"
+	"testing"
+)
 
 func TestJSONDB(t *testing.T) {
-	db, err := NewJSONDatabase()
+	var db backend.Driver
+	var err error
+	db, err = NewJSONDatabase("mock_path")
 	t.Run("DB does not error", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Unexpected error: %+v", err)
 		}
 	})
+
+	t.Run("I can ping my json", func(*testing.T) {
+		_ = db.Ping()
+	})
+
 }
