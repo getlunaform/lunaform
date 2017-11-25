fbc6202cbf05af423feed494f592299021a39923
Drew J. Sonne <drew.sonne@gmail.com>
2017-11-25 17:46:08 +0000

    Added basic test for db

--- backend/database/json_test.go
+++ backend/database/json_test.go
@@ -1,1 +1,12 @@
 package database
+
+import "testing"
+
+func TestJSONDB(t *testing.T) {
+	db, err := NewJSONDatabase()
+	t.Run("DB does not error", func(t *testing.T) {
+		if err != nil {
+			t.Errorf("Unexpected error: %+v", err)
+		}
+	})
+}
