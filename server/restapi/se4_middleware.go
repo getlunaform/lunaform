5e0190e5fbb7ce5bd310aabaed9079870405b0c7
Drew J. Sonne <drew.sonne@gmail.com>
2017-12-03 12:03:14 +0000

    Added goose4

---
+++ server/restapi/se4_middleware.go
@@ -0,0 +1,30 @@
+package restapi
+
+import (
+	"net/http"
+	"github.com/zeebox/goose4"
+	"strings"
+)
+
+// Middleware handles the "/service" prefix to comply with the SE4 prefix
+type Middleware struct {
+	handler http.Handler
+	SE4     goose4.Goose4
+}
+
+// NewMiddleware takes an http handler
+// to wrap and returns mutable Middleware object
+func NewMiddleware(h http.Handler) *Middleware {
+	return &Middleware{
+		handler: h,
+	}
+}
+
+// ServeHTTP wraps our requests and handles any calles to `/service*`.
+func (m *Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if strings.HasPrefix(r.URL.String(), "/service") {
+		m.SE4.ServeHTTP(w, r)
+	} else {
+		m.handler.ServeHTTP(w, r)
+	}
+}
