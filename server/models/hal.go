60f406a9cf47d0d34e3d858f6e48b8c418a9c593
Drew J. Sonne <drew.sonne@gmail.com>
2018-07-09 10:46:08 +0100

    Add clean functions

--- server/models/hal.go
+++ server/models/hal.go
@@ -5,11 +5,32 @@ type HalLinkable interface {
 }
 
 func (m *ResponseListResources) Clean() interface{} {
-	m.Links = nil
-	return m
+	return m.Embedded.Clean()
 }
 
 func (m *ResourceTfModule) Clean() interface{} {
 	m.Links = nil
 	return m
+}
+
+func (resource *Resource) Clean() interface{} {
+	return &Resource{
+		Name: resource.Name,
+	}
+}
+
+func (m *ResourceListTfModule) Clean() interface{} {
+	rscs := make([]interface{}, len(m.Resources))
+	for i, rsc := range m.Resources {
+		rscs[i] = rsc.Clean()
+	}
+	return rscs
+}
+
+func (list *ResourceList) Clean() interface{} {
+	rscs := make([]interface{}, len(list.Resources))
+	for i, rsc := range list.Resources {
+		rscs[i] = rsc.Clean()
+	}
+	return rscs
 }
