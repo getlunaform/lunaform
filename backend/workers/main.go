d159e9157da3dfaa00bb032385351b2364b5dbc2
Drew J. Sonne <drew.sonne@gmail.com>
2018-07-24 16:20:49 +0100

    Add scratch folder

--- backend/workers/main.go
+++ backend/workers/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/go-openapi/swag"
 	"github.com/getlunaform/go-terraform"
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
 const (
@@ -27,14 +29,17 @@ func (tap *TfActionPlan) Type() *string {
 }
 
 type TfAgentPool struct {
-	maxWorkers int
-	db         database.Database
-	pool       *workerpool.WorkerPool
+	maxWorkers    int
+	db            database.Database
+	pool          *workerpool.WorkerPool
+	scratchFolder string
 }
 
 func NewAgentPool(maxWorkers int) *TfAgentPool {
+	tempdir := os.TempDir()
 	return &TfAgentPool{
-		maxWorkers: maxWorkers,
+		maxWorkers:    maxWorkers,
+		scratchFolder: tempdir,
 	}
 }
 
@@ -55,22 +60,35 @@ func (p *TfAgentPool) WithDB(db database.Database) *TfAgentPool {
 func (p *TfAgentPool) DoPlan(a *TfActionPlan) {
 	p.pool.Submit(func() {
 
+		workingDir := filepath.Join(p.scratchFolder, "stack-"+a.Stack.ID)
+
+		params := &goterraform.TerraformPlanParams{
+			Out: swag.String(filepath.Join(workingDir, "deployment-"+a.Deployment.ID+".plan")),
+		}
+
+		action := goterraform.NewTerraformClient().
+			WithWorkingDirectory(workingDir).
+			Plan(params).
+			Init()
+
 		logs := goterraform.NewOutputLogs()
-
-		action := goterraform.NewTerraformClient().Plan().Init()
 		if err := action.InitLogger(logs); err != nil {
-
 			fmt.Printf("An error occured initialising task logger: " + err.Error())
 			return
 		}
 
+		if err := os.Mkdir(workingDir, os.ModeDir); err != nil {
+			logs.Error(err)
+			return
+		}
+
 		if err := action.Cmd.Start(); err != nil {
-			logs.Stderr(err.Error())
+			logs.Error(err)
 			return
 		}
 
 		if err := action.Cmd.Wait(); err != nil {
-			logs.Stderr(err.Error())
+			logs.Error(err)
 			return
 		}
 
